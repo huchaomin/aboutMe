c21236e29a3552f57cc675f2a5175256d2fedfbd
mulinzi2 <iisa_peter@163.com>
2017-05-26 00:25:44 +0800

    666

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,14 @@
 
 jwplayer.key="N2Z47h5lEkdFZFPwrPS0j5p+FH3UmaACS13lzg==";
-var jwplayerHeight=$("#video-wrapper").css("height");
-var jwplayerWidth=$("#video-wrapper").css("width");
-Player= jwplayer("video").setup({
+Player= jwplayer("videoRabbit").setup({
     "stretching": "uniform",
-    "primary":"html5",
-    "preload":"none",
+    "primary":"flash",
+    "preload":"auto",
     "flashplayer":"scripts/jwplayer-7.9.1/jwplayer.flash.swf",
     "controls":true,
     "nextupoffset":"-4",                /*最后几秒钟出现下一个视频弹出框*/
-    "width":jwplayerWidth,
-    "height":jwplayerHeight,
+    "width":$(".container>div").css("width"),
+    "height":$(".container>div").css("height"),
 
     //"file":"videos/rabbit/tinyRabbit1.mp4",     //一个文件的写法
     //"image":"images/tinyRabbit1.jpg",
@@ -50,7 +48,150 @@ Player= jwplayer("video").setup({
         "nextUp":"下一个兔子视频"
     }
 });
-
+Player= jwplayer("springFestivalOf2014").setup({
+    "stretching": "uniform",
+    "primary":"flash",
+    "preload":"auto",
+    "flashplayer":"scripts/jwplayer-7.9.1/jwplayer.flash.swf",
+    "controls":true,
+    "nextupoffset":"-4",                /*最后几秒钟出现下一个视频弹出框*/
+    "width":$(".container>div").css("width"),
+    "height":$(".container>div").css("height"),
+    "playlist": [
+        {
+            "file": "videos/The Spring Festival of 2014/20141230.MP4",
+            "image":"images/20141230.jpg",
+            "title": "2014年大年三十",
+            "mediaid": "1"
+        },
+        {
+            "file": "videos/The Spring Festival of 2014/20150307.MP4",
+            "image":"images/201503072.jpg",
+            "title": "2015年三月七日",
+            "mediaid": "2"
+        }
+    ]
+});
+Player= jwplayer("springFestivalOf2015").setup({
+    "stretching": "uniform",
+    "primary":"flash",
+    "preload":"auto",
+    "flashplayer":"scripts/jwplayer-7.9.1/jwplayer.flash.swf",
+    "controls":true,
+    "nextupoffset":"-4",                /*最后几秒钟出现下一个视频弹出框*/
+    "width":$(".container>div").css("width"),
+    "height":$(".container>div").css("height"),
+    "playlist": [
+        {
+            "file": "videos/The Spring Festival of 2015/201512301.MOV",
+            "image":"images/201512301.jpg",
+            "title": "2015年大年三十1",
+            "mediaid": "1"
+        },
+        {
+            "file": "videos/The Spring Festival of 2015/201512302.MOV",
+            "image":"images/201512301.jpg",
+            "title": "2015年大年三十2",
+            "mediaid": "2"
+        },
+        {
+            "file": "videos/The Spring Festival of 2015/201512303.MOV",
+            "image":"images/201512301.jpg",
+            "title": "2015年大年三十3",
+            "mediaid": "3"
+        }
+    ]
+});
+Player= jwplayer("springFestivalOf2016").setup({
+    "stretching": "uniform",
+    "primary":"flash",
+    "preload":"auto",
+    "flashplayer":"scripts/jwplayer-7.9.1/jwplayer.flash.swf",
+    "controls":true,
+    "nextupoffset":"-4",                /*最后几秒钟出现下一个视频弹出框*/
+    "width":$(".container>div").css("width"),
+    "height":$(".container>div").css("height"),
+    "playlist": [
+        {
+            "file": "videos/The Spring Festival of 2016/201612301.MOV",
+            "image":"images/20161230.jpg",
+            "title": "2016年大年三十1",
+            "mediaid": "1"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201612302.MOV",
+            "image":"images/20161230.jpg",
+            "title": "2016年大年三十2",
+            "mediaid": "2"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201612303.MOV",
+            "image":"images/20161230.jpg",
+            "title": "2016年大年三十3",
+            "mediaid": "3"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701021.MOV",
+            "image":"images/20170102.jpg",
+            "title": "2017年正月初二1",
+            "mediaid": "4"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701022.MOV",
+            "image":"images/20170102.jpg",
+            "title": "2017年正月初二2",
+            "mediaid": "5"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701023.MOV",
+            "image":"images/20170102.jpg",
+            "title": "2017年正月初二3",
+            "mediaid": "6"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701031.MOV",
+            "image":"images/20170103.jpg",
+            "title": "2017年正月初三1",
+            "mediaid": "7"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701032.MOV",
+            "image":"images/20170103.jpg",
+            "title": "2017年正月初三2",
+            "mediaid": "8"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701061.MOV",
+            "image":"images/20170106.jpg",
+            "title": "2017年正月初六1",
+            "mediaid": "9"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701062.MOV",
+            "image":"images/20170106.jpg",
+            "title": "2017年正月初六2",
+            "mediaid": "10"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701063.MOV",
+            "image":"images/20170106.jpg",
+            "title": "2017年正月初六3",
+            "mediaid": "11"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701064.MOV",
+            "image":"images/20170106.jpg",
+            "title": "2017年正月初六4",
+            "mediaid": "12"
+        },
+        {
+            "file": "videos/The Spring Festival of 2016/201701065.MOV",
+            "image":"images/20170106.jpg",
+            "title": "2017年正月初六5",
+            "mediaid": "13"
+        }
+    ]
+});
 //小火箭
 $(function() {
     var e = $("#rocket-to-top"),
